b7663ebc4dbc6de537d24fb642858c9d0ad37801
Jason McGuirk <jason.mcguirk@gmail.com>
2019-12-14 08:51:14 -0800

    Less embarassing approach (6min -> 14ms runtime)

--- problem-14b.go
+++ problem-14b.go
@@ -10,32 +10,52 @@ func (this *Problem14B) Solve() {
 	Log.Info("Problem 14B solver beginning!")
 
 
-
 	system := &ChemicalReactionSystem{};
 	err := system.Load("source-data/input-day-14b.txt");
 	if(err != nil){
 		Log.FatalError(err);
 	}
-
-	reserves := 1000000000000;
-
-
 	outputProduct := "FUEL";
 	inputDesired := "ORE";
+	reserves := 1000000000000;
 
-	// Pretty naive approach, but it's fast enough
+	search := 1;
+	stepSize := 2;
+	for{
+		res, err := system.GetTotalInputRequired(outputProduct, search, inputDesired);
+		if(err != nil){
+			Log.FatalError(err);
+		}
+		if(search < 0){
+			break;
+		}
+		if(res < reserves){
+			if(stepSize == 1){
+				Log.Info("Exhausted reserves - max we can produce is %d", search);
+				break;
+			}
+			stepSize *= 2;
+			search += stepSize;
+		} else {
+			stepSize = stepSize / 2;
+			search -= stepSize;
+		}
+	}
+
+	/*
+
 	for i := 1; i < reserves; i++{
 		res, err := system.GetTotalInputRequired(outputProduct, i, inputDesired);
 		if(err != nil){
 			Log.FatalError(err);
 		}
 		if(res >= reserves){
-			Log.Info("Exhausted reserves - we can produce a max of %d fuel", i - 1 );
+			Log.Info("Exhausted reserves - we can product max is %d", i - 1 );
 			break;
 		} else{
 			Log.Info("%d - %d", i, res);
 		}
-	}
+	}*/
 
 
 
